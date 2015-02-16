7bb25bc300371b432673c0c1eca155ea14ff1499
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-02-17 01:05:00 +0900

    add status code handling

--- sensu/request.go
+++ sensu/request.go
@@ -1,13 +1,20 @@
 package sensu
 
 import (
+	"fmt"
+	"os"
 	"strings"
 )
 
 func PostRequest(check string, subscriber string) string {
 	body := `{"check":"` + check + `","subscribers":["` + subscriber + `"]}`
 	payload := strings.NewReader(body)
+
 	_, status := postAPI("/request", payload)
+	if status != 202 {
+		fmt.Println(httpStatus(status))
+		os.Exit(1)
+	}
 
 	return httpStatus(status)
 }
--- sensu/resolve.go
+++ sensu/resolve.go
@@ -1,13 +1,20 @@
 package sensu
 
 import (
+	"fmt"
+	"os"
 	"strings"
 )
 
 func PostResolve(client string, check string) string {
 	body := `{"client":"` + client + `","check":"` + check + `"}`
 	payload := strings.NewReader(body)
+
 	_, status := postAPI("/resolve", payload)
+	if status != 202 {
+		fmt.Println(httpStatus(status))
+		os.Exit(1)
+	}
 
 	return httpStatus(status)
 }
